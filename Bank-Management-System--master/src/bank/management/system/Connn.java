e948e9c4e71ef8a71b32c08d517ed5d5a08e8ed0
Sneha Rastogi <iit2023154@iiita.ac.in>
2024-11-15 04:40:52 +0530

    Hidden SQL database credentials

--- Bank-Management-System--master/src/bank/management/system/Connn.java
+++ Bank-Management-System--master/src/bank/management/system/Connn.java
@@ -7,7 +7,7 @@ public class Connn {
     Statement statement;
     public Connn(){
         try{
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankSystem","Sneha","srnaeshta0606");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/(MySQLDatabase","User(root)","userPassword");
             statement = connection.createStatement();
         }catch (Exception e){
             e.printStackTrace();
